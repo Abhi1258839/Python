670c5eaf6b8bfee19057030d1ab29207ab9af884
Abhi1258839 <jindalabhimanyu1@gmail.com>
2026-06-04 08:03:06 -0700

    created player and movement

--- space_Dodge.py
+++ space_Dodge.py
@@ -3,28 +3,56 @@ import pygame
 import random
 
 WIDTH, HEIGHT = 1000, 800
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("bg.jpeg")
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60
+PLAYER_VEL = 5
+
+def draw(player):
+
     WIN.blit(BG, (0,0))
+    pygame.draw.rect(WIN, "red" , player)
     pygame.display.update()
     
 def main():
     run = True
+    
+    player = pygame.Rect(200, HEIGHT- PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
 
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        keys = pygame.key.get_pressed()
 
-        draw()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL <= WIDTH - player.width:
+            player.x += PLAYER_VEL
+
+        draw(player)
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
